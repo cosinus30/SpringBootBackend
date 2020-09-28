58b65e1b1700ca2998626b0212f2aa05c7ab419c
MArifSahin <mehmetarifsahin@etu.edu.tr>
2020-09-28 13:46:30 +0300

    Password encoder added.

--- src/main/java/com/innova/WebSecurityConfig.java
+++ src/main/java/com/innova/WebSecurityConfig.java
@@ -1,10 +1,13 @@
 package com.innova;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /*
  * Without writing this class
@@ -19,6 +22,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
--- src/main/java/com/innova/controller/UserController.java
+++ src/main/java/com/innova/controller/UserController.java
@@ -4,6 +4,7 @@ package com.innova.controller;
 import com.innova.domain.User;
 import com.innova.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,6 +15,9 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    PasswordEncoder encoder;
 
     @GetMapping("/")
     public List<User> getUsers(){
@@ -28,6 +32,7 @@ public class UserController {
     @PostMapping("/")
     public User postUser(@RequestBody User user){
         try {
+            user.setPassword(encoder.encode(user.getPassword()));
             return userService.save(user);
         }catch(Exception e){
             return null;
