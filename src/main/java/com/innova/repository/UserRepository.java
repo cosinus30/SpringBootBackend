37515ef92a18f2546a1e226aeb24f2bb59f1ac9b
Yunus Kerem Turk <krmtrk20@gmail.com>
2020-09-29 12:29:06 +0300

    Add UserRepository method signatures

--- src/main/java/com/innova/repository/UserRepository.java
+++ src/main/java/com/innova/repository/UserRepository.java
@@ -3,6 +3,12 @@ package com.innova.repository;
 import com.innova.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
-    
+    Optional<User> findByUsername(String username);
+
+    Boolean existByUsername(String username);
+
+    Boolean existByEmail(String email);
 }
