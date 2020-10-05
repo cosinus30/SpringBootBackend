b7e7b7a402e8b77243f7df9b8e9c37787fafc1c9
Unknown <y.turk@etu.edu.tr>
2020-10-05 10:48:36 +0300

    Change logic for invalid captcha
    Instead of exception throwing we send an error to client directly.

--- src/main/java/com/innova/aspect/CaptchaAspect.java
+++ src/main/java/com/innova/aspect/CaptchaAspect.java
@@ -9,13 +9,13 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Aspect
 @Component
@@ -31,17 +31,27 @@ public class CaptchaAspect {
 
     @Around("@annotation(RequiresCaptcha)")
     public Object validateCaptcha(ProceedingJoinPoint joinPoint) throws Throwable{
+
         HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
-            Attempt attempt = attemptRepository.findById(request.getRemoteAddr()).orElseThrow(()->
-                    new UsernameNotFoundException("User Not Ip addr: " + request.getRemoteAddr()));
-            if(attempt.getAttemptCounter() >= 3){
-                String captchaResponse = request.getHeader(CAPTCHA_HEADER_NAME);
-                boolean isValidCaptcha = captchaValidator.validateCaptcha(captchaResponse);
-                if(!isValidCaptcha){
-                    throw new ForbiddenException("Invalid captcha");
-                }
+        HttpServletResponse response = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getResponse();
+
+
+        Attempt attempt = attemptRepository.findById(request.getRemoteAddr()).orElseThrow(()->
+                new UsernameNotFoundException("User Not Ip addr: " + request.getRemoteAddr()));
+        if(attempt.getAttemptCounter() >= 3){
+
+            String captchaResponse = request.getHeader(CAPTCHA_HEADER_NAME);
+            boolean isValidCaptcha = captchaValidator.validateCaptcha(captchaResponse);
+            if(!isValidCaptcha){
+                response.sendError(HttpServletResponse.SC_EXPECTATION_FAILED, "Captcha was expected");
             }
+            else{
+                return joinPoint.proceed();
+            }
+        }
         return joinPoint.proceed();
+
+
     }
 
 
