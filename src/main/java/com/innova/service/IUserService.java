448a4e1392d1aaba5a0962664d2cb829777b0f69
Kerem <krmtrk20@gmail.com>
2020-10-07 11:50:07 +0300

    Remove verification logic done with entity model

--- src/main/java/com/innova/service/IUserService.java
+++
@@ -1,9 +0,0 @@
-package com.innova.service;
-
-import com.innova.model.User;
-import org.springframework.stereotype.Service;
-
-@Service
-public interface IUserService {
-    void createVerificationToken(User user, String token);
-}
--- src/main/resources/db/changelog/changes/verification_token.sql
+++
@@ -1,7 +0,0 @@
-create table if not exists "verification_token" (
-    id serial primary key,
-    created_date date not null,
-    expiry_date date not null,
-    token varchar(255) not null,
-    user_id int not null references cloud_users(id)
-);
