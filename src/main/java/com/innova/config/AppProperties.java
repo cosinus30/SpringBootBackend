6f889f15e65f3aed7891e94ea0706cde12f13582
Kerem <krmtrk20@gmail.com>
2020-10-19 12:00:36 +0300

    Fix null pointer error for AppProperties

--- src/main/java/com/innova/config/AppProperties.java
+++ src/main/java/com/innova/config/AppProperties.java
@@ -1,9 +1,11 @@
 package com.innova.config;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Component
 @ConfigurationProperties(prefix = "innova")
 public class AppProperties {
 
