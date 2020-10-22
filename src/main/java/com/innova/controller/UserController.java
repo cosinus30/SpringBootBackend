a3759e67785bca93b661c69dce9d84901d09614a
Kerem <krmtrk20@gmail.com>
2020-10-22 17:30:25 +0300

    Add change password functionality

--- src/main/java/com/innova/controller/UserController.java
+++ src/main/java/com/innova/controller/UserController.java
@@ -1,8 +1,9 @@
 package com.innova.controller;
 
 
+import com.innova.message.request.ChangePasswordForm;
 import com.innova.message.request.LogoutForm;
-import com.innova.message.request.changeForm;
+import com.innova.message.request.ChangeForm;
 import com.innova.model.TokenBlacklist;
 import com.innova.model.User;
 import com.innova.repository.TokenBlacklistRepository;
@@ -47,21 +48,18 @@ public class UserController {
     }
 
     @PutMapping("/edit")
-    public ResponseEntity<?> editUser(@Valid @RequestBody changeForm changeForm) {
+    public ResponseEntity<?> editUser(@Valid @RequestBody ChangeForm changeForm) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication.isAuthenticated() && !authentication.getPrincipal().equals("anonymousUser")) {
             UserDetailImpl userDetails = (UserDetailImpl) authentication.getPrincipal();
 
             User user = userRepository.findByUsername(userDetails.getUsername()).orElseThrow(() -> new RuntimeException("Fail! -> Cause: User cannot find"));
-            if (changeForm.getEmail() != null || changeForm.getPassword() != null || changeForm.getAge() != null || changeForm.getName() != null || changeForm.getLastname() != null || changeForm.getPhoneNumber() != null) {
+            if (changeForm.getEmail() != null || changeForm.getAge() != null || changeForm.getName() != null || changeForm.getLastname() != null || changeForm.getPhoneNumber() != null) {
                 if (changeForm.getEmail() != null) {
                     if (userRepository.existsByEmail(changeForm.getEmail())) {
                         return new ResponseEntity<String>("Email is already in use!", HttpStatus.BAD_REQUEST);
                     }
                     user.setEmail(changeForm.getEmail());
-                }
-                if (changeForm.getPassword() != null) {
-                    user.setPassword(passwordEncoder.encode(changeForm.getPassword()));
                 }
                 if (changeForm.getName() != null)
                     user.setName(changeForm.getName());
@@ -102,6 +100,51 @@ public class UserController {
             myMap.put("status", HttpStatus.OK.value());
             return new ResponseEntity(myMap, HttpStatus.OK);
         }
+    }
 
+    @PostMapping("/change-password")
+    public ResponseEntity<?> createNewPassword(@RequestBody ChangePasswordForm changePasswordForm){
+        Map<String, Object> myMap = new HashMap<>();
+        myMap.put("timestamp", new Date());
+        myMap.put("path", "api/auth/forgot-password");
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication.isAuthenticated() && !authentication.getPrincipal().equals("anonymousUser")) {
+            UserDetailImpl userDetails = (UserDetailImpl) authentication.getPrincipal();
+            User user = userRepository.findByUsername(userDetails.getUsername()).orElseThrow(() -> new RuntimeException("Fail! -> Cause: User cannot find"));
+            if (changePasswordForm.checkAllFieldsAreGiven(changePasswordForm)) {
+                myMap.put("error", "All fields should be given");
+                myMap.put("status", HttpStatus.BAD_REQUEST.value());
+                return new ResponseEntity(myMap, HttpStatus.BAD_REQUEST);
+            } else {
+                if(user.getPassword() != passwordEncoder.encode(changePasswordForm.getOldPassword())){
+                    myMap.put("error", "Your old password is not correct");
+                    myMap.put("status", HttpStatus.BAD_REQUEST.value());
+                    return new ResponseEntity(myMap, HttpStatus.BAD_REQUEST);
+                }
+                else if(changePasswordForm.getNewPassword() != changePasswordForm.getNewPasswordConfirmation()){
+                    myMap.put("error", "Password fields does not match");
+                    myMap.put("status", HttpStatus.BAD_REQUEST.value());
+                    return new ResponseEntity(myMap, HttpStatus.BAD_REQUEST);
+                }
+                else if(changePasswordForm.getNewPassword() == changePasswordForm.getNewPasswordConfirmation()){
+                    user.setPassword(passwordEncoder.encode(changePasswordForm.getNewPassword()));
+                    userRepository.save(user);
+                    myMap.put("message", "Password successfully changed");
+                    myMap.put("status", HttpStatus.OK.value());
+                    return new ResponseEntity(myMap, HttpStatus.OK);
+                }
+                else{
+                    myMap.put("error", "Something is wrong");
+                    myMap.put("status", HttpStatus.BAD_REQUEST.value());
+                    return new ResponseEntity(myMap, HttpStatus.BAD_REQUEST);
+                }
+            }
+        }
+        else{
+            myMap.put("error", "Unauthorized access");
+            myMap.put("status", HttpStatus.UNAUTHORIZED.value());
+            return new ResponseEntity(myMap, HttpStatus.UNAUTHORIZED);
+        }
     }
 }
--- src/main/java/com/innova/message/request/ChangeForm.java
+++ src/main/java/com/innova/message/request/ChangeForm.java
@@ -4,22 +4,12 @@ import java.util.Set;
 
 import javax.validation.constraints.*;
 
-public class changeForm {
-
-    @NotBlank
-    @Size(min = 3, max = 50)
-    private String username;
+public class ChangeForm {
 
     @NotBlank
     @Size(max = 60)
     @Email
     private String email;
-
-    private Set<String> role;
-
-    @NotBlank
-    @Size(min = 6, max = 20)
-    private String password;
 
     @Size(min = 3, max = 25)
     private String name;
@@ -71,21 +61,5 @@ public class changeForm {
 
     public void setEmail(String email) {
         this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Set<String> getRole() {
-        return this.role;
-    }
-
-    public void setRole(Set<String> role) {
-        this.role = role;
     }
 }
