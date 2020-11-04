0e922a6b302921c01bde77cdbc74296d70bc361a
Kerem <krmtrk20@gmail.com>
2020-11-04 10:46:20 +0300

    Refactor returning response entity strategy

--- src/main/java/com/innova/controller/AuthenticationController.java
+++ src/main/java/com/innova/controller/AuthenticationController.java
@@ -1,19 +1,22 @@
 package com.innova.controller;
 
 import com.innova.aspect.RequiresCaptcha;
+import com.innova.constants.ErrorCodes;
+import com.innova.dto.request.ForgotPasswordForm;
+import com.innova.dto.request.LoginForm;
+import com.innova.dto.request.SignUpForm;
+import com.innova.dto.response.LoginResponse;
+import com.innova.dto.response.SuccessResponse;
 import com.innova.event.OnPasswordForgotEvent;
 import com.innova.event.OnRegistrationSuccessEvent;
 import com.innova.exception.AccountNotActivatedException;
+import com.innova.exception.BadRequestException;
 import com.innova.exception.ErrorWhileSendingEmailException;
 import com.innova.model.ActiveSessions;
 import com.innova.model.Attempt;
 import com.innova.model.Role;
 import com.innova.model.Roles;
 import com.innova.model.User;
-import com.innova.message.request.ForgotPasswordForm;
-import com.innova.message.request.LoginForm;
-import com.innova.message.request.SignUpForm;
-import com.innova.message.response.LoginResponse;
 import com.innova.repository.ActiveSessionsRepository;
 import com.innova.repository.AttemptRepository;
 import com.innova.repository.RoleRepository;
@@ -26,6 +29,7 @@ import com.innova.util.PasswordUtil;
 import io.jsonwebtoken.ExpiredJwtException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -74,39 +78,32 @@ public class AuthenticationController {
 
     @PostMapping("signin")
     @RequiresCaptcha
-    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginForm loginForm, HttpServletRequest request) throws IOException, AccountNotActivatedException {
-        Map<String, Object> myMap = new HashMap<>();
-        myMap.put("timestamp", new Date());
-        myMap.put("path", "api/auth/signin");
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginForm loginForm, HttpServletRequest request)
+            throws IOException, AccountNotActivatedException {
 
         if (loginForm.getPassword() == null || loginForm.getUsername() == null) {
-            myMap.put("error", "Username and password should be provided");
-            myMap.put("status", HttpStatus.BAD_REQUEST.value());
-            return new ResponseEntity<>(myMap, HttpStatus.BAD_REQUEST);
+            throw new BadRequestException("Username and password should be provided",
+                    ErrorCodes.USERNAME_AND_PASSWORD.getValue());
         }
 
         Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(
-                        loginForm.getUsername(),
-                        loginForm.getPassword()
-                )
-        );
+                new UsernamePasswordAuthenticationToken(loginForm.getUsername(), loginForm.getPassword()));
 
-        User user = userRepository.findByUsername(loginForm.getUsername()).orElseThrow(() -> new RuntimeException("Fail! -> Cause: User Role not find."));
+        User user = userRepository.findByUsername(loginForm.getUsername())
+                .orElseThrow(() -> new BadRequestException("User with given username could not found", ErrorCodes.NO_SUCH_USER.getValue()));
 
         if (!user.isEnabled()) {
             throw new AccountNotActivatedException("Account has not been activated.");
         }
 
         UserDetailImpl userPrincipal = UserDetailImpl.build(user);
-
         SecurityContextHolder.getContext().setAuthentication(authentication);
-
         String accessToken = jwtProvider.generateJwtToken(userPrincipal);
         String refreshToken = jwtProvider.generateRefreshToken(authentication, loginForm.isRememberMe());
-
         UserDetailImpl userDetails = (UserDetailImpl) authentication.getPrincipal();
-        List<String> roles = userDetails.getAuthorities().stream().map(item -> item.getAuthority()).collect(Collectors.toList());
+        List<String> roles = userDetails.getAuthorities().stream().map(item -> item.getAuthority())
+                .collect(Collectors.toList());
+
         if (attemptRepository.existsByIp(request.getRemoteAddr())) {
             Attempt attempt = attemptRepository.findById(request.getRemoteAddr()).get();
             attempt.setAttemptCounter(0);
@@ -114,27 +111,19 @@ public class AuthenticationController {
         }
 
         String userAgent = request.getHeader("User-Agent") == null ? "Not known" : request.getHeader("User-Agent");
-
-        ActiveSessions activeSession = new ActiveSessions(
-                refreshToken,
-                accessToken,
-                userAgent,
-                LocalDateTime.ofInstant(jwtProvider.getExpiredDateFromJwt(refreshToken, "refresh").toInstant(), ZoneId.systemDefault()),
-                LocalDateTime.ofInstant(jwtProvider.getIssueDateFromJwt(refreshToken, "refresh").toInstant(), ZoneId.systemDefault())
-        );
+        ActiveSessions activeSession = new ActiveSessions(refreshToken, accessToken, userAgent,
+                LocalDateTime.ofInstant(jwtProvider.getExpiredDateFromJwt(refreshToken, "refresh").toInstant(),
+                        ZoneId.systemDefault()),
+                LocalDateTime.ofInstant(jwtProvider.getIssueDateFromJwt(refreshToken, "refresh").toInstant(),
+                        ZoneId.systemDefault()));
 
         activeSession.setUser(user);
 
         activeSessionsRepository.save(activeSession);
 
-        return ResponseEntity.ok(new LoginResponse(accessToken,
-                refreshToken,
-                userDetails.getId(),
-                userDetails.getUsername(),
-                userDetails.getEmail(),
-                roles,
-                userDetails.getName(),
-                userDetails.getLastName()));
+        return ResponseEntity
+                .ok(new LoginResponse(accessToken, refreshToken, userDetails.getId(), userDetails.getUsername(),
+                        userDetails.getEmail(), roles, userDetails.getName(), userDetails.getLastName()));
     }
 
     @PostMapping("sign-up")
@@ -143,24 +132,18 @@ public class AuthenticationController {
         myMap.put("timestamp", new Date());
         myMap.put("path", "api/auth/sign-up");
         if (userRepository.existsByUsername(signUpForm.getUsername())) {
-            myMap.put("status", HttpStatus.BAD_REQUEST.value());
-            myMap.put("error", "Username is already taken!");
-            return new ResponseEntity<>(myMap, HttpStatus.BAD_REQUEST);
+            throw new BadRequestException("Username is already taken!", ErrorCodes.USERNAME_ALREADY_TAKEN.getValue());
         }
-
         if (userRepository.existsByEmail(signUpForm.getEmail())) {
-            myMap.put("status", HttpStatus.BAD_REQUEST.value());
-            myMap.put("error", "Email is already in use!");
-            return new ResponseEntity<>(myMap, HttpStatus.BAD_REQUEST);
+            throw new BadRequestException("Email is already in use!", ErrorCodes.EMAIL_ALREADY_TAKEN.getValue());
         }
 
         if (!PasswordUtil.isValidPassword(signUpForm.getPassword())) {
-            myMap.put("status", HttpStatus.BAD_REQUEST.value());
-            myMap.put("error", "Password is not valid. It should have at least one uppercase, lowercase letter, number. Min length is 6");
-            return new ResponseEntity<>(myMap, HttpStatus.BAD_REQUEST);
+            throw new BadRequestException("Password is not valid", ErrorCodes.PASSWORD_NOT_VALID.getValue());
         }
 
-        User user = new User(signUpForm.getUsername(), signUpForm.getEmail(), passwordEncoder.encode(signUpForm.getPassword()));
+        User user = new User(signUpForm.getUsername(), signUpForm.getEmail(),
+                passwordEncoder.encode(signUpForm.getPassword()));
         Set<Role> roles = new HashSet<>();
         Role userRole = roleRepository.findByRole(Roles.ROLE_USER)
                 .orElseThrow(() -> new RuntimeException("Fail! -> Cause: User Role not find."));
@@ -179,27 +162,25 @@ public class AuthenticationController {
     }
 
     @GetMapping("/confirmRegistration")
-    public ResponseEntity<?> confirmRegistration(@RequestParam("token") String token, HttpServletRequest request) throws URISyntaxException {
+    public ResponseEntity<?> confirmRegistration(@RequestParam("token") String token, HttpServletRequest request)
+            throws URISyntaxException {
         if (token == null) {
-            //Token is empty redirect to error or something
-            return ResponseEntity.status(HttpStatus.SEE_OTHER)
-                    .location(URI.create("http://localhost:4200"))
-                    .build();
+            // Token is empty redirect to error or something
+            return ResponseEntity.status(HttpStatus.SEE_OTHER).location(URI.create("http://localhost:4200")).build();
         }
 
         if (token != null && jwtProvider.validateJwtToken(token, "verification", request)) {
             String username = jwtProvider.getSubjectFromJwt(token, "verification");
-            User user = userRepository.findByEmail(username).orElseThrow(() -> new RuntimeException("Fail! -> Cause: User Role not find."));
+            User user = userRepository.findByEmail(username)
+                    .orElseThrow(() -> new BadRequestException("User with given email could not found", ErrorCodes.NO_SUCH_USER.getValue()));
             user.setEnabled(true);
             userRepository.save(user);
 
-            return ResponseEntity.status(HttpStatus.SEE_OTHER)
-                    .location(URI.create("http://localhost:4200/mailsuccess"))
+            return ResponseEntity.status(HttpStatus.SEE_OTHER).location(URI.create("http://localhost:4200/mailsuccess"))
                     .build();
         } else {
             // Token is not valid
-            return ResponseEntity.status(HttpStatus.SEE_OTHER)
-                    .location(URI.create("http://localhost:4200/mailerror"))
+            return ResponseEntity.status(HttpStatus.SEE_OTHER).location(URI.create("http://localhost:4200/mailerror"))
                     .build();
         }
     }
@@ -210,13 +191,12 @@ public class AuthenticationController {
         myMap.put("timestamp", new Date());
         myMap.put("path", "api/auth/refresh-token");
         if (token == null) {
-            myMap.put("error", "Token cannot be empty");
-            myMap.put("status", HttpStatus.BAD_REQUEST.value());
-            return new ResponseEntity<>(myMap, HttpStatus.BAD_REQUEST);
+            throw new BadRequestException("Token cannot be empty", ErrorCodes.TOKEN_CANNOT_BE_EMPTY.getValue());
         }
         try {
             String email = jwtProvider.getSubjectFromJwt(token, "refresh");
-            User user = userRepository.findByEmail(email).orElseThrow(() -> new RuntimeException("Fail! -> Cause: Email not found."));
+            User user = userRepository.findByEmail(email)
+                    .orElseThrow(() -> new BadRequestException("User with given email could not found", ErrorCodes.NO_SUCH_USER.getValue()));
             if (!user.isEnabled()) {
                 throw new AccountNotActivatedException("Account has not been activated.");
             }
@@ -244,21 +224,14 @@ public class AuthenticationController {
 
     @PostMapping("forgot-password")
     public ResponseEntity<?> forgotPassword(@RequestBody ForgotPasswordForm forgotPasswordForm) {
-        Map<String, Object> myMap = new HashMap<>();
-        myMap.put("timestamp", new Date());
-        myMap.put("path", "api/auth/forgot-password");
         String email = forgotPasswordForm.getEmail();
-
         if (!userRepository.existsByEmail(email)) {
-            myMap.put("error", "No such user");
-            myMap.put("status", HttpStatus.BAD_REQUEST.value());
-            return new ResponseEntity<>(myMap, HttpStatus.BAD_REQUEST);
+            throw new BadRequestException("User with given email could not found", ErrorCodes.NO_SUCH_USER.getValue());
         } else {
             try {
                 eventPublisher.publishEvent(new OnPasswordForgotEvent(email));
-                myMap.put("message", "Email successfuly sent.");
-                myMap.put("status", HttpStatus.OK.value());
-                return new ResponseEntity<>(myMap, HttpStatus.OK);
+                SuccessResponse response = new SuccessResponse(HttpStatus.OK,"Email successfuly sent");
+                return new ResponseEntity<>(response, new HttpHeaders(), response.getStatus());
             } catch (Exception re) {
                 throw new ErrorWhileSendingEmailException(re.getMessage());
             }
@@ -267,20 +240,13 @@ public class AuthenticationController {
 
     @GetMapping("send-email")
     public ResponseEntity<?> sendNewEmail(@RequestParam("email") String email) {
-        Map<String, Object> myMap = new HashMap<>();
-        myMap.put("timestamp", new Date());
-        myMap.put("path", "api/auth/send-email");
         try {
-            User user = userRepository.findByEmail(email).orElseThrow(() -> new RuntimeException("Fail! -> Cause: Email not found"));
+            User user = userRepository.findByEmail(email)
+                    .orElseThrow(() -> new BadRequestException("User with given email could not found", ErrorCodes.NO_SUCH_USER.getValue()));
             eventPublisher.publishEvent(new OnRegistrationSuccessEvent(user, "/api/auth"));
-            myMap.put("message", "Email successfuly sent.");
-            myMap.put("status", HttpStatus.OK.value());
-            return new ResponseEntity<>(myMap, HttpStatus.OK);
-        } catch (RuntimeException e) {
-            myMap.put("error", "No such user");
-            myMap.put("status", HttpStatus.BAD_REQUEST.value());
-            return new ResponseEntity<>(myMap, HttpStatus.BAD_REQUEST);
-        } catch (Exception re) {
+            SuccessResponse response = new SuccessResponse(HttpStatus.OK,"Email successfuly sent");
+            return new ResponseEntity<>(response, new HttpHeaders(), response.getStatus());
+        }catch (Exception re) {
             throw new ErrorWhileSendingEmailException(re.getMessage());
         }
     }
