dcf4db6e9fa36c026b0c2cea3553b250ca4cfd6a
Kerem <krmtrk20@gmail.com>
2020-11-12 19:33:43 +0300

    Add view table

--- src/main/resources/db/changelog/changes/articles_006.sql
+++ src/main/resources/db/changelog/changes/articles_006.sql
@@ -6,8 +6,8 @@ create table if not exists  "articles" (
     release_date timestamp without time zone NOT NULL,
     content_type text NOT NULL,
     read_time int NOT NULL,
-    author int not null references cloud_users(id)
+    author int not null references cloud_users(id),
+    bookmark_count int DEFAULT 0,
+    like_count int DEFAULT 0,
+    view_count int DEFAULT 0
 );
-
-ALTER TABLE "articles" ADD COLUMN "bookmark_count" int DEFAULT 0;
-ALTER TABLE "articles" DROP COLUMN "bookmark count"
